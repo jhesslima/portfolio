f8630368b7f6d5317a19c3eb19c089795697c152
jhesslima <jessicamendesdelima09@gmail.com>
2025-12-18 20:53:23 -0300

    fix: corrigindo hover dos cards and update: apagando página de contato e acrescentando scroll

--- script.js
+++ script.js
@@ -13,12 +13,14 @@ const cards = document.querySelectorAll('[data-card]');
 cards.forEach(card => {
   card.addEventListener('mouseenter', () => {
     cards.forEach(c => {
-      if (c !== card) c.classList.add('hovered');
+      if (c !== card) {
+        c.classList.add('blur');
+      }
     });
   });
 
   card.addEventListener('mouseleave', () => {
-    cards.forEach(c => c.classList.remove('hovered'));
+    cards.forEach(c => c.classList.remove('blur'));
   });
 });
 
@@ -26,18 +28,18 @@ cards.forEach(card => {
 
   const text = document.querySelector('.universities-text');
 
-  let lastScrollY = window.scrollY;
+  if (text) {
+    let lastScrollY = window.scrollY;
 
-  window.addEventListener('scroll', () => {
-    const currentScrollY = window.scrollY;
+    window.addEventListener('scroll', () => {
+      const currentScrollY = window.scrollY;
 
-    if (currentScrollY > lastScrollY) {
-      // scroll para baixo → mostra
-      text.classList.add('visible');
-    } else {
-      // scroll para cima → esconde
-      text.classList.remove('visible');
-    }
+      if (currentScrollY > lastScrollY) {
+        text.classList.add('visible');
+      } else {
+        text.classList.remove('visible');
+      }
 
-    lastScrollY = currentScrollY;
-  });
+      lastScrollY = currentScrollY;
+    });
+  }
